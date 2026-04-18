85ce67e0674497811642a27ee94928829b08f4ff
Gourish Prajapati <gourishp18@gmail.com>
2026-04-18 23:33:32 +0530

    debug forgot password

--- services/utils/src/sendEmail.ts
+++ services/utils/src/sendEmail.ts
@@ -29,30 +29,55 @@
 
 //   console.log(`📧 Email sent to ${to}`);
 // };
-import nodemailer from "nodemailer";
-import dotenv from "dotenv";
+// import nodemailer from "nodemailer";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
+// export const sendEmail = async ({ to, subject, html }: any) => {
+
+//   if (!process.env.SMTP_USER || !process.env.SMTP_PASS) {
+//     throw new Error(
+//       `ENV CHECK FAILED | USER=${process.env.SMTP_USER} | PASS_EXISTS=${!!process.env.SMTP_PASS}`
+//     );
+//   }
+
+//   const transporter = nodemailer.createTransport({
+//     host: "smtp.gmail.com",
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: process.env.SMTP_USER,
+//       pass: process.env.SMTP_PASS,
+//     },
+//   });
+
+//   await transporter.sendMail({
+//     from: process.env.SMTP_USER,
+//     to,
+//     subject,
+//     html,
+//   });
+// };
+import nodemailer from "nodemailer";
+
 export const sendEmail = async ({ to, subject, html }: any) => {
 
-  if (!process.env.SMTP_USER || !process.env.SMTP_PASS) {
-    throw new Error(
-      `ENV CHECK FAILED | USER=${process.env.SMTP_USER} | PASS_EXISTS=${!!process.env.SMTP_PASS}`
-    );
-  }
+  // hardcode temporarily for debugging
+  const user = "ayancoe877@gmail.com";
+  const pass = "huuuyknkwlcjrlxx";
 
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
     port: 465,
     secure: true,
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user,
+      pass,
     },
   });
 
   await transporter.sendMail({
-    from: process.env.SMTP_USER,
+    from: user,
     to,
     subject,
     html,
