d5644a4743f23a44a65daf409602f129a0176702
Gourish Prajapati <gourishp18@gmail.com>
2026-04-18 23:07:33 +0530

    debug forgot password

--- services/utils/src/sendEmail.ts
+++ services/utils/src/sendEmail.ts
@@ -3,10 +3,9 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-console.log("SMTP_USER =", process.env.SMTP_USER);
-console.log("SMTP_PASS exists =", !!process.env.SMTP_PASS);
+
 export const sendEmail = async ({ to, subject, html }: any) => {
-  console.log("SMTP_USER =", process.env.SMTP_USER);
+ console.log("SMTP_USER =", process.env.SMTP_USER);
  console.log("SMTP_PASS exists =", !!process.env.SMTP_PASS);
   const transporter = nodemailer.createTransport({
     host: "smtp.gmail.com",
